e05d92cfe5ac59df3188fc8887f26e44719d3279
zmoore-usgs <zmoore@usgs.gov>
2017-10-05 12:22:16 -0500

    MLR-171: Removed unused imports

--- src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
+++ src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
@@ -1,7 +1,5 @@
 package gov.usgs.wma.mlrauthserver.config;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
