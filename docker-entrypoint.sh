b6374cb93bfa1ea141477ea4b54c1b71ff4414c1
zmoore-usgs <zmoore@usgs.gov>
2017-10-12 16:44:42 -0500

    MLR-171: Updated the dockerfile to not include automatic keystore generation, and rather expect that a pre-built keystore is passed into the container via a secret or config.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -x
 
-keytool -genkeypair -alias $keystoreDefaultKey -keyalg RSA -dname "CN=water-auth,OU=Unit,O=Organization,L=City,S=State,C=US" -keypass $keystorePassword -keystore $keystoreLocation -storepass $keystorePassword -noprompt
-keytool -genkeypair -alias $keystoreTokenSigningKey -keyalg RSA -dname "CN=water-auth,OU=Unit,O=Organization,L=City,S=State,C=US" -keystore $keystoreLocation -storepass $keystorePassword -noprompt
-keytool -genkeypair -alias tomcat -keyalg RSA -dname "CN=water-auth,OU=Unit,O=Organization,L=City,S=State,C=US" -keystore $keystoreLocation -storepass $keystorePassword -noprompt
-
 if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
 
