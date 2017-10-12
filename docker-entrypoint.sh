acef360349224821e4ffdde9db6fdec428386056
zmoore-usgs <zmoore@usgs.gov>
2017-10-12 16:56:26 -0500

    MLR-171: Fixed an issue where multiple keys were created using the same alias, causing an error.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -x
 if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
 
-if [ -n "$samlIdpHost" ] ; then openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > saml.crt; fi
-if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file saml.crt -alias saml -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
+if [ -n "$samlIdpHost" ] ; then openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > samlidp.crt; fi
+if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file samlidp.crt -alias samlidp -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
 
 java -Djava.security.egd=file:/dev/./urandom -jar app.war
 
