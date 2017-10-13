2b602c068d0e4bc10e5b5aeee3fadcf5f6ce468c
zmoore-usgs <zmoore@usgs.gov>
2017-10-13 13:02:57 -0500

    MLR-171: Added additional logging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,19 @@ certFileLocation = '/run/secrets/waterauthserver_WATER_AUTH_KEYS'
 
 keytool -v -importkeystore -srckeystore $certFileLocation -srcstoretype PKCS12 -srcstorepass $keystorePassword -destkeystore $keystoreLocation -deststoretype JKS -deststorepass $keystorePassword -noprompt
 
+echo _________ Finished loading certs from PKCS12 _________
+
 if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
 
 if [ -n "$samlIdpHost" ] ; then openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > samlidp.crt; fi
 if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file samlidp.crt -alias samlidp -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
+
+echo _________ Finished adding cert from $samlIdpHost _________
+
+keytool -list -keystore $keystoreLocation -storepass $keystorePassword
+
+echo _________ Launching Spring Application _________
 
 java -Djava.security.egd=file:/dev/./urandom -jar app.war
 
