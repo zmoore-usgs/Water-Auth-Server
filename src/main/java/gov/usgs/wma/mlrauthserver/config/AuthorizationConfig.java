ce202339c0839c30909f97b2975f0e8d6402ad03
zmoore-usgs <zmoore@usgs.gov>
2017-10-24 12:20:52 -0500

    MLR-153: Initial cut of Oauth JWT generation integratino with SAML.

--- src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
+++ src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
@@ -1,9 +1,11 @@
 package gov.usgs.wma.mlrauthserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResourceLoader;
@@ -12,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
@@ -19,47 +22,43 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.saml.SAMLEntryPoint;
 
 @Configuration
 @EnableAuthorizationServer
+@EnableResourceServer
 public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-
-	private int accessTokenValiditySeconds = 10000;
-	private int refreshTokenValiditySeconds = 30000;
-
-	@Value("${security.oauth2.resource.id}")
-	private String resourceId;
 	@Value("${keystoreLocation}")
 	private String keystorePath;
-	@Value("${keystoreDefaultKey}")
-	private String keystoreDefaultKey;
 	@Value("${keystoreTokenSigningKey}")
 	private String keystoreTokenSigningKey;
 	@Value("${keystorePassword}")
 	private String keystorePassword;
-
+	
 	@Autowired
+	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
-
+	
+	@Autowired
+	@Qualifier("samlEntryPoint")
+	private SAMLEntryPoint samlEntryPoint;
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
 		oauthServer
-		// we're allowing access to the token only for clients with 'ROLE_TRUSTED_CLIENT' authority
-		.tokenKeyAccess("hasAuthority('ROLE_TRUSTED_CLIENT')")
-		.checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
+		.authenticationEntryPoint(samlEntryPoint)
+		.tokenKeyAccess("permitAll()")
+		.checkTokenAccess("isAuthenticated()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 		.withClient("trusted-app")
-		.authorizedGrantTypes("client_credentials", "password", "refresh_token")
-		.authorities("ROLE_TRUSTED_CLIENT")
-		.scopes("read", "write")
-		.resourceIds(resourceId)
-		.accessTokenValiditySeconds(accessTokenValiditySeconds)
-		.refreshTokenValiditySeconds(refreshTokenValiditySeconds)
-		.secret("secret");
+		.authorizedGrantTypes("authorization_code", "access_token")
+		.scopes("user_info")
+		.secret("secret")
+		.autoApprove(true) ;
 	}
 
 	@Bean
