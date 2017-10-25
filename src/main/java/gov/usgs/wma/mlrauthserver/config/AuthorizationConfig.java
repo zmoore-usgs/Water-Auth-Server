7ddd2672d9684b7090a3bbd60ed87c6014e23723
zmoore-usgs <zmoore@usgs.gov>
2017-10-25 09:46:32 -0500

    MLR-153: Additional tweaks and cleanup related to SAMl --> Oauth2 token conversion.

--- src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
+++ src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
@@ -26,7 +25,6 @@ import org.springframework.security.saml.SAMLEntryPoint;
 
 @Configuration
 @EnableAuthorizationServer
-@EnableResourceServer
 public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 	@Value("${keystoreLocation}")
 	private String keystorePath;
