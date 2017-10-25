9fe14364a0da90ee21c3da7c9301dc74238f7840
zmoore-usgs <zmoore@usgs.gov>
2017-10-25 09:56:28 -0500

    MLR-153: Slight code cleanup and re-enabling of HTTPS.

--- src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
+++ src/main/java/gov/usgs/wma/mlrauthserver/config/AuthorizationConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResourceLoader;
