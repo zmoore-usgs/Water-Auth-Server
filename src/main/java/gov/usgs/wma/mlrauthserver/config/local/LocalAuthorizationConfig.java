eb3a7cea6ea5f57fda50cae5a2396e153ab12050
Steinich, Dave R. <drsteini@usgs.gov>
2017-10-26 15:43:45 -0500

    MLR-145 - Local Dev via config

--- src/main/java/gov/usgs/wma/mlrauthserver/config/local/LocalAuthorizationConfig.java
+++ src/main/java/gov/usgs/wma/mlrauthserver/config/local/LocalAuthorizationConfig.java
@@ -1,7 +1,6 @@
-package gov.usgs.wma.mlrauthserver.config;
+package gov.usgs.wma.mlrauthserver.config.local;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,20 +12,14 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableAuthorizationServer
-@Profile("default")
-public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+@Profile("localDev")
+public class LocalAuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
-	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
-
-	@Autowired
-	@Qualifier("samlEntryPoint")
-	private AuthenticationEntryPoint samlEntryPoint;
 
 	@Autowired
 	private TokenStore tokenStore;
@@ -38,7 +31,6 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
 		oauthServer
-			.authenticationEntryPoint(samlEntryPoint)
 			.tokenKeyAccess("permitAll()")
 			.checkTokenAccess("isAuthenticated()");
 	}
@@ -47,19 +39,20 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 			.withClient("nwis")
-			.authorizedGrantTypes("authorization_code", "access_token", "refresh_token")
+			.authorizedGrantTypes("authorization_code", "access_token", "refresh_token", "client_credentials", "password")
 			.scopes("user_info")
 			.secret("changeMe")
-			.autoApprove(true) ;
+			.autoApprove(true)
+			;
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
-		.authenticationManager(authenticationManager)
-		.tokenServices(tokenServices)
-		.tokenStore(tokenStore)
-		.accessTokenConverter(accessTokenConverter);
+			.authenticationManager(authenticationManager)
+			.tokenServices(tokenServices)
+			.tokenStore(tokenStore)
+			.accessTokenConverter(accessTokenConverter);
 	}
 
 }
