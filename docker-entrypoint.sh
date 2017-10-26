a855084228ab93fcc4959afd7eb42649f89a7edd
Stanley Kaymen <skaymen@usgs.gov>
2017-10-26 14:57:17 -0500

    Get the correct 3 keypairs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,15 @@ set -x
 
 keystorePassword=`cat $KEYSTORE_PASSWORD_FILE`
 
-openssl pkcs12 -export -in $WATER_AUTH_CERT -inkey $WATER_AUTH_KEY -name water_auth_pkcs12 -out wa_pkcs12.p12
-keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore wa_pkcs12.p12 -srcstoretype PKCS12
-keytool -v -import -alias bundle -trustcacerts -file wa_pkcs12.p12 -keystore $keystoreLocation
+openssl pkcs12 -export -in $OAUTH_CERT -inkey $OAUTH_KEY -name $keystoreDefaultKey -out oauth.p12
+openssl pkcs12 -export -in $SAML_CERT -inkey $SAML_KEY -name $keystoreTokenSigningKey -out saml.p12
+openssl pkcs12 -export -in $TOMCAT_CERT -inkey $TOMCAT_KEY -name tomcat -out tomcat.p12
+
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore oauth.p12 -srcstoretype PKCS12
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore saml.p12 -srcstoretype PKCS12
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore tomcat.p12 -srcstoretype PKCS12
+
+# keytool -v -import -alias bundle -trustcacerts -file wa_pkcs12.p12 -keystore $keystoreLocation
 
 if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
