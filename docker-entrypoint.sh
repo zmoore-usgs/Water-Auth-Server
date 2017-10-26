3f159feaf90a97dc44b6e8a035d8635fb4d22253
Stanley Kaymen <skaymen@usgs.gov>
2017-10-26 13:12:04 -0500

    Add -v flag

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -x
 keystorePassword=`cat $KEYSTORE_PASSWORD_FILE`
 
 openssl pkcs12 -export -in $WATER_AUTH_CERT -inkey $WATER_AUTH_KEY -name water_auth_pkcs12 -out wa_pkcs12.p12
-keytool -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore wa_pkcs12.p12 -srcstoretype PKCS12
-keytool -import -alias bundle -trustcacerts -file wa_pkcs12.p12 -keystore $keystoreLocation
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore wa_pkcs12.p12 -srcstoretype PKCS12
+keytool -v -import -alias bundle -trustcacerts -file wa_pkcs12.p12 -keystore $keystoreLocation
 
 if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
