ddc854f97bb6d5b3e015622f984f5acd9317bec3
Andrew Yan <ayan@usgs.gov>
2017-10-30 10:52:11 -0500

    readd support for source p12 passwords

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,20 +3,19 @@ set -x
 
 keystorePassword=`cat $KEYSTORE_PASSWORD_FILE`
 
-openssl pkcs12 -export -in $waterauthserver_TOKEN_CERT_path -inkey $waterauthserver_TOKEN_KEY_path -name $keystoreDefaultKey -out oauth.p12
-openssl pkcs12 -export -in $waterauthserver_SAML_CERT_path -inkey $waterauthserver_SAML_KEY_path -name $keystoreTokenSigningKey -out saml.p12
-openssl pkcs12 -export -in $waterauthserver_TOMCAT_CERT_path -inkey $waterauthserver_TOMCAT_KEY_path -name tomcat -out tomcat.p12
+openssl pkcs12 -export -in $waterauthserver_TOKEN_CERT_path -inkey $waterauthserver_TOKEN_KEY_path -name $keystoreDefaultKey -out oauth.p12 -password pass:$keystorePassword
+openssl pkcs12 -export -in $waterauthserver_SAML_CERT_path -inkey $waterauthserver_SAML_KEY_path -name $keystoreTokenSigningKey -out saml.p12 -password pass:$keystorePassword
+openssl pkcs12 -export -in $waterauthserver_TOMCAT_CERT_path -inkey $waterauthserver_TOMCAT_KEY_path -name tomcat -out tomcat.p12 -password pass:$keystorePassword
 
-keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore oauth.p12 -srcstoretype PKCS12
-keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore saml.p12 -srcstoretype PKCS12
-keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore tomcat.p12 -srcstoretype PKCS12
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore oauth.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 --noprompt
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore saml.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 --noprompt
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore tomcat.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 --noprompt
 
 if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
 
 if [ -n "$samlIdpHost" ] ; then openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > samlidp.crt; fi
-if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file samlidp.crt -alias samlidp -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
-
+if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file saxkcd
 keytool -list -keystore $keystoreLocation -storepass $keystorePassword
 java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$keystorePassword -jar app.jar
 
