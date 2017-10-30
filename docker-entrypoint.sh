782957362a73583fa52d59cd47ff9373d517ec81
Andrew Yan <ayan@usgs.gov>
2017-10-30 11:13:28 -0500

    fixed messed up code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,8 +15,8 @@ if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/
 if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
 
 if [ -n "$samlIdpHost" ] ; then openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > samlidp.crt; fi
-if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file saxkcd
-keytool -list -keystore $keystoreLocation -storepass $keystorePassword
+if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file samlidp.crt -alias samlidp -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
+
 java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$keystorePassword -jar app.jar
 
 exec $?
