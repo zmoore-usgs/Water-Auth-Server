3632f6803f3c533b592ac2b9be7a97af02afdea6
zmoore-usgs <zmoore@usgs.gov>
2017-11-01 13:49:36 -0500

    MLR-219: Change the sAML key environment variable name in the entrypoint script.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,8 +9,8 @@ fi
 
 keystorePassword=`cat $KEYSTORE_PASSWORD_FILE`
 
-openssl pkcs12 -export -in $waterauthserver_TOKEN_CERT_path -inkey $waterauthserver_TOKEN_KEY_path -name $keystoreTokenSigningKey -out oauth.p12 -password pass:$keystorePassword
-openssl pkcs12 -export -in $waterauthserver_SAML_CERT_path -inkey $waterauthserver_SAML_KEY_path -name $keystoreOAuthKey -out saml.p12 -password pass:$keystorePassword
+openssl pkcs12 -export -in $waterauthserver_TOKEN_CERT_path -inkey $waterauthserver_TOKEN_KEY_path -name $keystoreOAuthKey -out oauth.p12 -password pass:$keystorePassword
+openssl pkcs12 -export -in $waterauthserver_SAML_CERT_path -inkey $waterauthserver_SAML_KEY_path -name $keystoreSAMLKey -out saml.p12 -password pass:$keystorePassword
 openssl pkcs12 -export -in $waterauthserver_TOMCAT_CERT_path -inkey $waterauthserver_TOMCAT_KEY_path -name tomcat -out tomcat.p12 -password pass:$keystorePassword
 
 keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -deststoretype JKS -srckeystore oauth.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 --noprompt
