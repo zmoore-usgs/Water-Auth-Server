367a271c68b682277392aff7be8d4c8921e6873f
Zack Moore <zmoore@usgs.gov>
2018-09-12 10:45:58 -0500

    Additional tweaks

--- src/main/java/gov/usgs/wma/mlrauthserver/config/SAMLSecurityConfig.java
+++ src/main/java/gov/usgs/wma/mlrauthserver/config/SAMLSecurityConfig.java
@@ -261,8 +261,8 @@ public class SAMLSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public SAMLDefaultLogger samlLogger() {
 		SAMLDefaultLogger logger = new SAMLDefaultLogger();
-		logger.setLogMessages(logMessages);
-		logger.setLogErrors(logErrors);
+		logger.setLogMessages(logMessages.booleanValue());
+		logger.setLogErrors(logErrors.booleanValue());
 		return logger;
 	}
 
