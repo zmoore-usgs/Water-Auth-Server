e475c4c7904def428a32f63b744632109a1799ce
Zack Moore <zmoore@usgs.gov>
2019-06-18 17:38:36 -0500

    Changes to work with the latest Spring Boot Base image.

--- docker/launch-app.sh
+++ docker/launch-app.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
 # Add cert for signing created OAuth2 tokens
+keystoreLocation=${SERVER_SSL_KEYSTORE:?}
+keystorePassword=${SERVER_SSL_KEYSTOREPASSWORD:?}
+
 if [ -n "${TOKEN_CERT_PATH}" ] && [ -f "${TOKEN_CERT_PATH}" ]; then
-  openssl pkcs12 -export -in $TOKEN_CERT_PATH -inkey $TOKEN_KEY_PATH -name $keystoreOAuthKey -out oauth.p12 -password pass:$keystorePassword
-  keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -deststoretype PKCS12 -srckeystore oauth.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 -noprompt
+  # Build PEM file
+  cat "${TOKEN_KEY_PATH}" > "$HOME/token.pem"
+  cat "${TOKEN_CERT_PATH}" >> "$HOME/token.pem"
+
+  # Import the PEM
+  openssl pkcs12 -export -in "$HOME/token.pem" -inkey "$TOKEN_KEY_PATH" -name "$keystoreOAuthKey" -out "$HOME/token.pkcs12" -password "pass:${keystorePassword}"
+  keytool -v -importkeystore -deststorepass "$keystorePassword" -destkeystore "$keystoreLocation" -deststoretype PKCS12 -srckeystore "$HOME/token.pkcs12" -srcstorepass "$keystorePassword" -srcstoretype PKCS12 -noprompt
 fi
 
 # Add cert for signing SAML communication with DOI
 if [ -n "${SAML_CERT_PATH}" ] && [ -f "${SAML_CERT_PATH}" ]; then
-  openssl pkcs12 -export -in $SAML_CERT_PATH -inkey $SAML_KEY_PATH -name $keystoreSAMLKey -out saml.p12 -password pass:$keystorePassword
-  keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -deststoretype PKCS12 -srckeystore saml.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 -noprompt
+  # Build PEM file
+  cat "${SAML_KEY_PATH}" > "$HOME/saml.pem"
+  cat "${SAML_CERT_PATH}" >> "$HOME/saml.pem"
+
+  # Import the PEM
+  openssl pkcs12 -export -in "$HOME/saml.pem" -inkey "$SAML_KEY_PATH" -name "$keystoreSAMLKey" -out "$HOME/saml.pkcs12" -password "pass:${keystorePassword}"
+  keytool -v -importkeystore -deststorepass "$keystorePassword" -destkeystore "$keystoreLocation" -deststoretype PKCS12 -srckeystore "$HOME/saml.pkcs12" -srcstorepass "$keystorePassword" -srcstoretype PKCS12 -noprompt
 fi
 
 # Add cert served by the SAML IDP
 if [ -n "${samlIdpHost}" ]; then
   openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > samlidp.crt;
-  keytool  -importcert -file samlidp.crt -alias samlidp -keystore $keystoreLocation -storepass $keystorePassword -noprompt;
+  keytool  -importcert -file samlidp.crt -alias samlidp -keystore "$keystoreLocation" -storepass "$keystorePassword" -noprompt;
 fi
 
 # Set correct Spring Launch Args if we are running local dev mode
@@ -29,4 +42,4 @@ fi
 # Most spring apps we build don't load the keystore via FileSystemResourceLoader, but Water Auth needs to for the SAML stuff
 PREFIXED_KEYSTORE_LOCATION="file:$keystoreLocation"
 
-java -server -Djava.security.egd=file:/dev/./urandom -DkeystoreLocation=$PREFIXED_KEYSTORE_LOCATION -jar app.jar $SPRING_APP_ARGS "$@"
+java -server -Djava.security.egd=file:/dev/./urandom -DkeystoreLocation=$PREFIXED_KEYSTORE_LOCATION -DkeystorePassword=$keystorePassword -jar app.jar $SPRING_APP_ARGS "$@"
